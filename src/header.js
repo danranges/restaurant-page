4b58c3ec0a9547d76448b22d8cb805696677abe7
Dan Ranges <danranges@gmail.com>
2021-05-11 20:59:50 +0200

    Add responsive nav

--- src/header.js
+++ src/header.js
@@ -17,8 +17,55 @@ function renderHeader() {
                            <div class="bar3"></div>`
     header.appendChild(hamburger)
 
+    const largeNav = document.createElement('div')
+    largeNav.classList.add('large-nav')
+    header.appendChild(largeNav)
 
-    hamburger.addEventListener('click', () => hamburger.classList.toggle('change'))
+    const container = document.createElement('div')
+    content.appendChild(container)
+
+    const mobileNav = document.createElement('div')
+    mobileNav.classList.add('mobile-nav')
+    content.appendChild(mobileNav)
+
+    const about = document.createElement('div')
+    about.classList.add('nav')
+    about.innerHTML = "About"
+    
+    const nursery = document.createElement('div')
+    nursery.classList.add('nav')
+    nursery.innerHTML = 'Nursery'
+    
+    const contact = document.createElement('div')
+    contact.classList.add('nav')
+    contact.innerHTML = 'Contact'
+
+    function widthCheck() {
+        console.log(document.body.clientWidth)
+        if (document.body.clientWidth <= 600) {
+            mobileNav.appendChild(about)
+            mobileNav.appendChild(nursery)
+            mobileNav.appendChild(contact)
+        } else {
+            largeNav.appendChild(about)
+            largeNav.appendChild(nursery)
+            largeNav.appendChild(contact)
+        }
+    }
+    
+    function mobileNavVisibility() {
+        hamburger.classList.toggle('change')
+        if (mobileNav.style.display === 'block') {
+            mobileNav.style.display = 'none'
+        } else {
+            mobileNav.style.display = 'block'
+        }
+    }
+
+    widthCheck()
+
+    hamburger.addEventListener('click', mobileNavVisibility)
+    window.addEventListener('resize', widthCheck)
 }
 
 export default renderHeader
