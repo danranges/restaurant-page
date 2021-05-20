64cb2d3279e8734967e77d855ae18dfa5347a209
Dan Ranges <danranges@gmail.com>
2021-05-20 21:59:07 +0200

    Add contact methods text/divs

--- src/contact.js
+++ src/contact.js
@@ -20,13 +20,30 @@ function renderContact () {
     const contactCopy = document.createElement('div')
     contactCopy.classList.add('contact-copy')
     contactCopy.innerText = 'Should you have questions about plant care, choice, or ordering, we are here to help. ' +
-                            'You can call us during business hours or email at any time. We look forward to hearing from you.'
+                            'You can call or visit us during business hours or email at any time. ' + 
+                            'We look forward to hearing from you.'
     contactHeaderContainer.appendChild(contactCopy)
 
     const contactMethodsContainer = document.createElement('div')
     contactMethodsContainer.classList.add('methods-container')
     content.appendChild(contactMethodsContainer)
 
+    const phoneContact = document.createElement('div')
+    phoneContact.classList.add('contact-method')
+    phoneContact.innerHTML += `Call us: <br> 867 53 090`
+    contactMethodsContainer.appendChild(phoneContact)
+
+    const emailContact = document.createElement('div')
+    emailContact.classList.add('contact-method')
+    emailContact.innerHTML += 'Email us: <br> KinderGarden@example.com'
+    contactMethodsContainer.appendChild(emailContact)
+
+    const visitContact = document.createElement('div')
+    visitContact.classList.add('contact-method')
+    visitContact.innerHTML += `Visit us:<br> 
+                               Kulebønnerveien 101<br>
+                               7770 Flatanger, Norge`
+    contactMethodsContainer.appendChild(visitContact)
 }
 
 export default renderContact
