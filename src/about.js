0d3e35f61d7c24d96eb8d7e18de2ce438fe5d31c
Dan Ranges <danranges@gmail.com>
2021-05-22 19:25:07 +0200

    Add contact icons and responsive CSS

--- src/about.js
+++ src/about.js
@@ -6,7 +6,7 @@ function renderAbout() {
     const container = document.getElementById('container')
 
     const aboutCopy = document.createElement('div')
-    aboutCopy.innerText = `No matter if it's a giant elephant ear or a tiny polka dot plant, few things breathe life into a room like a houseplant. 
+    aboutCopy.innerText = `No matter if it's a giant set of elephant ears or a tiny polka dot plant, few things breathe life into a room like a houseplant. 
                            
                            When your heart yearns for a bit more green, stop by one of our Classrooms or visit our virtual Nursery School, where you can find everything from alocasias to ZZ plants, birds of paradise to whale fins.`
     aboutCopy.classList.add('text-copy')
